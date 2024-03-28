0ae0ba3e3a129a04d4127a03ce8f424cc7cfedd4
Tyler <tylermartel@outlook.com>
2024-03-28 19:44:03 -0400

    add CPURegisters struct

--- CPU.cs
+++ CPU.cs
@@ -2,8 +2,57 @@
 
 namespace Atem
 {
+    internal struct CPURegisters
+    {
+        public byte IR;
+        public byte A, B, C, D, E, H, L;
+        public ushort PC, SP;
+        public byte W, Z;
+
+        public ushort BC
+        {
+            get { return ((ushort)0).SetHighByte(B).SetLowByte(C); }
+            set
+            {
+                B = value.GetHighByte();
+                C = value.GetLowByte();
+            }
+        }
+
+        public ushort DE
+        {
+            get { return ((ushort)0).SetHighByte(D).SetLowByte(E); }
+            set
+            {
+                D = value.GetHighByte();
+                E = value.GetLowByte();
+            }
+        }
+
+        public ushort HL
+        {
+            get { return ((ushort)0).SetHighByte(H).SetLowByte(L); }
+            set
+            {
+                H = value.GetHighByte();
+                L = value.GetLowByte();
+            }
+        }
+
+        public ushort WZ
+        {
+            get { return ((ushort)0).SetHighByte(W).SetLowByte(Z); }
+            set
+            {
+                W = value.GetHighByte();
+                Z = value.GetLowByte();
+            }
+        }
+    }
+
     internal class CPU
     {
+        private CPURegisters _registers;
         private Bus _bus;
 
         public CPU(Bus bus)
