0c9a36c4ad7a4b4238d0baac68176d63e2035eea
Tyler <tylermartel@outlook.com>
2024-03-28 19:05:37 -0400

    add Bus class

---
+++ Bus.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Atem
+{
+    internal class Bus
+    {
+        private BootROM _bootROM;
+
+        public void LoadBootROM(string filepath, bool enabled = true)
+        {
+            _bootROM = new BootROM(filepath, enabled);
+        }
+
+        public byte Read(ushort address)
+        {
+            byte block = address.GetHighByte();
+            byte offset = address.GetLowByte();
+
+            if (block <= 0x00 && _bootROM.Enabled)
+            {
+                return _bootROM[offset];
+            }
+
+            return 0xFF;
+        }
+
+        public void Write(ushort address, byte value)
+        {
+            byte block = address.GetHighByte();
+            byte offset = address.GetLowByte();
+
+            if (block <= 0x00 && _bootROM.Enabled) { }
+        }
+    }
+}
