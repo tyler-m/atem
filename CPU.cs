5f1e4fd3ec264dd657e3a3424d22a511de4e2671
Tyler <tylermartel@outlook.com>
2024-03-28 19:16:16 -0400

    add CPU class

---
+++ CPU.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Atem
+{
+    internal class CPU
+    {
+        private Bus _bus;
+
+        public CPU(Bus bus)
+        {
+            _bus = bus;
+        }
+
+        private byte Read(ushort address)
+        {
+            return _bus.Read(address);
+        }
+
+        private void Write(ushort address, byte value)
+        {
+            _bus.Write(address, value);
+        }
+
+        public void Clock()
+        {
+
+        }
+    }
+}
