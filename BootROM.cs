4e531395904f887474c12c82986e7d06705eefb6
Tyler <tylermartel@outlook.com>
2024-03-28 18:51:25 -0400

    add BootROM class

---
+++ BootROM.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+
+namespace Atem
+{
+    internal class BootROM
+    {
+        private byte[] _rom = new byte[0x100];
+
+        public bool Enabled;
+
+        public byte this[ushort address]
+        {
+            get { return _rom[address]; }
+        }
+
+        public BootROM(string filepath = "", bool enabled = true)
+        {
+            Enabled = enabled;
+
+            if (filepath != "")
+            {
+                Load(filepath);
+            }
+        }
+
+        private void Load(string filepath)
+        {
+            if (File.Exists(filepath))
+            {
+                _rom = File.ReadAllBytes(filepath);
+            }
+        }
+    }
+}
