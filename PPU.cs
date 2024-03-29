c77420dc168bfc33a6cb7ac1412331203c0138c1
Tyler <tylermartel@outlook.com>
2024-03-29 19:39:42 -0400

    add PPU class

---
+++ PPU.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Atem
+{
+    internal class PPU
+    {
+        private byte[] _vram = new byte[0x2000];
+
+        public void WriteVRAM(ushort address, byte value)
+        {
+            _vram[address & 0x1FFF] = value;
+        }
+
+        public byte ReadVRAM(ushort address)
+        {
+            return _vram[address & 0x1FFF];
+        }
+    }
+}
