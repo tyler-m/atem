a47a87fffe060a6401949a45963030b67f7b1b80
Tyler <tylermartel@outlook.com>
2024-04-27 20:54:00 -0400

    add audio filter interface and implement high pass filter

---
+++ Core/Audio/Filter/HighPassFilter.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Atem.Core.Audio.Filter
+{
+    /// <summary>
+    /// Useful for centering waveform to 0.0
+    /// </summary>
+    internal class HighPassFilter : IAudioFilter
+    {
+        float _prevLeftChannelIn = 0.0f;
+        float _prevLeftChannelOut = 0.0f;
+        float _prevRightChannelIn = 0.0f;
+        float _prevRightChannelOut = 0.0f;
+        float _factor = 1.0f;
+
+        public HighPassFilter(float factor)
+        {
+            _factor = factor;
+        }
+
+        public (float, float) Filter(float leftChannelIn, float rightChannelIn)
+        {
+            float leftChannelOut = _prevLeftChannelOut * _factor + leftChannelIn - _prevLeftChannelIn;
+            _prevLeftChannelIn = leftChannelIn;
+            _prevLeftChannelOut = leftChannelOut;
+
+            float rightChannelOut = _prevRightChannelOut * _factor + rightChannelIn - _prevRightChannelIn;
+            _prevRightChannelIn = rightChannelIn;
+            _prevRightChannelOut = rightChannelOut;
+
+            return (leftChannelOut, rightChannelOut);
+        }
+    }
+}
---
+++ Core/Audio/Filter/IAudioFilter.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Atem.Core.Audio.Filter
+{
+    internal interface IAudioFilter
+    {
+        public (float leftChannel, float rightChannel) Filter(float leftChannel, float rightChannel);
+    }
+}
