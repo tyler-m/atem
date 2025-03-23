658f3e992e05e48bebf03749ccc23b8007d98374
Tyler <tylermartel@outlook.com>
2025-03-23 10:47:04 -0400

    add RTC support to MBC3

--- src/Atem.Core/Memory/Mapper/MBC3.cs
+++ src/Atem.Core/Memory/Mapper/MBC3.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace Atem.Core.Memory.Mapper
 {
     internal class MBC3 : IMapper
@@ -9,8 +11,27 @@ namespace Atem.Core.Memory.Mapper
         private bool _ramEnable = false;
         private int _romBank = 1;
         private int _ramBank = 0;
+        private RTC _rtc = new RTC();
+        private RTC _rtcLatched = new RTC();
+        private byte _latch = 0;
 
         public byte[] RAM { get => _ram; set => _ram = value; }
+
+        public void LoadRTCFromSaveData(byte[] data)
+        {
+            byte[] timestampData = new byte[4];
+            Array.Copy(data, 40, timestampData, 0, 4);
+
+            // RTC values are conventionally saved in little endian
+            if (!BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(timestampData);
+            }
+
+            int unixTimestamp = BitConverter.ToInt32(timestampData);
+
+            _rtc = new RTC(data[0], data[4], data[8], data[12].SetBit(8, data[16].GetBit(0)), data[16].GetBit(7), data[16].GetBit(6), unixTimestamp);
+        }
 
         public void Init(byte type, byte[] rom, int ramSize)
         {
@@ -36,24 +57,78 @@ namespace Atem.Core.Memory.Mapper
 
         public void WriteRAM(ushort address, byte value)
         {
-            if (_ramBank <= 0x03 && _ramEnable)
+            if (_ramEnable)
             {
-                int adjustedAddress = address + 0x2000 * _ramBank;
-                if (adjustedAddress < _ram.Length)
+                if (_ramBank <= 0x03)
                 {
-                    _ram[adjustedAddress] = value;
+                    int adjustedAddress = address + 0x2000 * _ramBank;
+                    if (adjustedAddress < _ram.Length)
+                    {
+                        _ram[adjustedAddress] = value;
+                    }
+                }
+                else // RTC register write
+                {
+                    if (_ramBank == 0x08)
+                    {
+                        _rtc.Seconds = value;
+                    }
+                    else if (_ramBank == 0x09)
+                    {
+                        _rtc.Minutes = value;
+                    }
+                    else if (_ramBank == 0x0A)
+                    {
+                        _rtc.Hours = value;
+                    }
+                    else if (_ramBank == 0x0B)
+                    {
+                        _rtc.Day = (_rtc.Day & 0b100000000) | value;
+                    }
+                    else if (_ramBank == 0x0C)
+                    {
+                        _rtc.Day = (_rtc.Day & 0xFF) | (value.GetBit(0).Int() << 8);
+                        _rtc.Halt = value.GetBit(6);
+                        _rtc.DayCarry = value.GetBit(7);
+                    }
                 }
             }
         }
 
         public byte ReadRAM(ushort address)
         {
-            if (_ramBank <= 0x03 && _ramEnable)
+            if (_ramEnable)
             {
-                int adjustedAddress = address + 0x2000 * _ramBank;
-                if (adjustedAddress < _ram.Length)
+                if (_ramBank <= 0x03)
                 {
-                    return _ram[adjustedAddress];
+                    int adjustedAddress = address + 0x2000 * _ramBank;
+                    if (adjustedAddress < _ram.Length)
+                    {
+                        return _ram[adjustedAddress];
+                    }
+                }
+                else // RTC register read
+                {
+                    if (_ramBank == 0x08)
+                    {
+                        return (byte)_rtcLatched.Seconds;
+                    }
+                    else if (_ramBank == 0x09)
+                    {
+                        return (byte)_rtcLatched.Minutes;
+                    }
+                    else if (_ramBank == 0x0A)
+                    {
+                        return (byte)_rtcLatched.Hours;
+                    }
+                    else if (_ramBank == 0x0B)
+                    {
+                        _rtcLatched.Day = (byte)_rtc.Day;
+                    }
+                    else if (_ramBank == 0x0C)
+                    {
+                        return (byte)(((_rtcLatched.Day >> 8) & 0b1) | (_rtcLatched.Halt.Int() << 6) | (_rtcLatched.DayCarry.Int() << 7));
+                    }
                 }
             }
 
@@ -64,7 +139,7 @@ namespace Atem.Core.Memory.Mapper
         {
             if (address <= 0x1FFF)
             {
-                if (value == 0xA)
+                if (value == 0x0A)
                 {
                     _ramEnable = true;
                 }
@@ -88,8 +163,48 @@ namespace Atem.Core.Memory.Mapper
             }
             else if (address <= 0x7FFF)
             {
+                if (value == 0x00)
+                {
+                    _latch = 0x00;
+                }
+                else if (value == 0x01)
+                {
+                    if (_latch == 0x00)
+                    {
+                        _rtcLatched = new RTC(_rtc);
+                        _rtcLatched.Halt = true;
+                    }
 
+                    _latch = 0x01;
+                }
             }
+        }
+
+        public byte[] ExportSave()
+        {
+            byte[] saveFile = new byte[RAM.Length + 48];
+            byte[] rtcData = new byte[48];
+
+            rtcData[0] = (byte)_rtc.Seconds;
+            rtcData[4] = (byte)_rtc.Minutes;
+            rtcData[8] = (byte)_rtc.Hours;
+            rtcData[12] = (byte)_rtc.Day;
+            rtcData[16].SetBit(0, ((ushort)_rtc.Day).GetBit(8));
+            rtcData[16].SetBit(6, _rtc.Halt);
+            rtcData[16].SetBit(7, _rtc.DayCarry);
+
+            byte[] timestampData = BitConverter.GetBytes((int)DateTimeOffset.UtcNow.ToUnixTimeSeconds());
+
+            // RTC values are conventionally saved in little endian
+            if (!BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(timestampData);
+            }
+
+            Array.Copy(timestampData, 0, rtcData, 40, 4);
+            Array.Copy(RAM, saveFile, RAM.Length);
+            Array.Copy(rtcData, 0, saveFile, RAM.Length, rtcData.Length);
+            return saveFile;
         }
     }
 }
