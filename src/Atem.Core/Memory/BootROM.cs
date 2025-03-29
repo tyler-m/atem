a95528f14250734a9468700d80b540c48eb70175
Tyler <tylermartel@outlook.com>
2025-03-28 23:28:16 -0400

    clean up boot rom

--- src/Atem.Core/Memory/BootROM.cs
+++ src/Atem.Core/Memory/BootROM.cs
@@ -1,26 +1,23 @@
 ﻿using System.IO;
 
-namespace Atem
+namespace Atem.Core.Memory
 {
     internal class BootROM
     {
         private byte[] _rom = new byte[0x100];
+        private bool _enabled;
 
-        public bool Enabled;
+        public bool Enabled { get => _enabled; set => _enabled = value; }
+
+        public BootROM(string filepath, bool enabled = true)
+        {
+            _enabled = enabled;
+            Load(filepath);
+        }
 
         public byte Read(ushort address)
         {
             return _rom[address & 0xFF];
-        }
-
-        public BootROM(string filepath = "", bool enabled = true)
-        {
-            Enabled = enabled;
-
-            if (filepath != "")
-            {
-                Load(filepath);
-            }
         }
 
         private void Load(string filepath)
