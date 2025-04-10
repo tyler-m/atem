bed217721a114a716d0fd03fe20ff3686e45c41e
Tyler <tylermartel@outlook.com>
2025-04-09 23:17:21 -0400

    add test project

---
+++ src/Atem.Test/Config/ConfigFileStoreTests.cs
@@ -0,0 +1,19 @@
+﻿using Atem.Config;
+
+namespace Atem.Test.Config
+{
+    public class ConfigFileStoreTests
+    {
+        [Fact]
+        public void Load_CreatesDefaultConfig_WhenConfigFileDoesNotExist()
+        {
+            string tempFilePath = Path.GetTempFileName();
+            File.Delete(tempFilePath);
+            ConfigFileStore store = new(tempFilePath);
+
+            store.Load();
+
+            Assert.True(File.Exists(tempFilePath));
+        }
+    }
+}
