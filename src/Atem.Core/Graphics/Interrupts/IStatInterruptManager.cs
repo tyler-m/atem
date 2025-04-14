bf35d04707e8ee5f93f71786758e4d3fda58d6e4
Tyler <tylermartel@outlook.com>
2025-04-14 12:07:29 -0400

    add stat interrupt manager interface

---
+++ src/Atem.Core/Graphics/Interrupts/IStatInterruptManager.cs
@@ -0,0 +1,15 @@
+﻿using Atem.Core.State;
+
+namespace Atem.Core.Graphics.Interrupts
+{
+    public interface IStatInterruptManager : IStateful
+    {
+        public bool InterruptOnOAM { get; set; }
+        public bool InterruptOnVerticalBlank { get; set; }
+        public bool InterruptOnHorizontalBlank { get; set; }
+        public bool InterruptOnLineY { get; set; }
+        public byte LineYToCompare { get; set; }
+        public bool CurrentlyOnLineY { get; }
+        public void UpdateLineYCompare();
+    }
+}
