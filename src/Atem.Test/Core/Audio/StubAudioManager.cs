6fefeb456f62b072ba7bbe673a7a29efb05474d1
Tyler <tylermartel@outlook.com>
2025-04-16 11:52:51 -0400

    update stub audio manager

--- src/Atem.Test/Core/Audio/StubAudioManager.cs
+++ src/Atem.Test/Core/Audio/StubAudioManager.cs
@@ -1,10 +1,13 @@
 ﻿using Atem.Core.Audio;
+using Atem.Core.Audio.Channel;
 
 namespace Atem.Test.Core.Audio
 {
     internal class StubAudioManager : IAudioManager
     {
         public float VolumeFactor { get; set; }
+
+        public IList<IAudioChannel> Channels => throw new NotImplementedException();
 
         public event Action<byte[]>? OnFullAudioBuffer;
     }
