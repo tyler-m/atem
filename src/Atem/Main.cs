4499a492510ae91258a79d8a891617b5e56aa65d
Tyler <tylermartel@outlook.com>
2025-04-27 16:58:20 -0400

    load cimgui library manually

--- src/Atem/Main.cs
+++ src/Atem/Main.cs
@@ -1,4 +1,7 @@
-﻿using Atem.Core;
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using Atem.Core;
 using Atem.Views.MonoGame;
 
 namespace Atem
@@ -6,6 +9,41 @@ namespace Atem
     internal class Program
     {
         static void Main(string[] args)
+        {
+            LoadLibraries();
+            Start();
+        }
+
+        private static void LoadLibraries()
+        {
+            string assemblyDirectoryPath = Path.GetDirectoryName(AppContext.BaseDirectory);
+
+            string[] libraries = [];
+            string systemId = string.Empty;
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                libraries = ["cimgui.dll"];
+                systemId = "win-x64";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                libraries = ["libcimgui.so"];
+                systemId = "linux-x64";
+            }
+
+            foreach (string library in libraries)
+            {
+                string libraryFilePath = Path.Combine(assemblyDirectoryPath, "runtimes", systemId, "native", library);
+
+                if (File.Exists(libraryFilePath))
+                {
+                    NativeLibrary.Load(libraryFilePath);
+                }
+            }
+        }
+
+        private static void Start()
         {
             Emulator emulator = new();
             ViewStarter.Start(emulator);
