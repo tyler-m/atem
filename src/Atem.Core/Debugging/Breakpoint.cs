a7ccf75eeb9b640f6243c361bc286e02e0eba54c
Tyler <tylermartel@outlook.com>
2025-04-27 19:20:14 -0400

    clean up breakpoint

--- src/Atem.Core/Debugging/Breakpoint.cs
+++ src/Atem.Core/Debugging/Breakpoint.cs
@@ -4,14 +4,12 @@ namespace Atem.Core.Debugging
 {
     public class Breakpoint : IComparable<Breakpoint>
     {
-        private ushort _address;
         private bool _enabled;
-        private int _hitCount;
 
-        public ushort Address { get => _address; set => _address = value; }
         public bool Enabled { get => _enabled; set => _enabled = value; }
         public ref bool EnabledRef { get => ref _enabled; }
-        public int HitCount { get => _hitCount; set => _hitCount = value; }
+        public ushort Address { get; set; }
+        public int HitCount { get; set; }
 
         public Breakpoint(ushort address, bool enabled = true)
         {
