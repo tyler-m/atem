d773be6ea7766b06ed2d535c6b9e7e417e485be9
Tyler <tylermartel@outlook.com>
2025-05-06 12:53:36 -0400

    move some emulator creation logic to factories

---
+++ src/Atem/Factories/EmulatorFactory.cs
@@ -0,0 +1,28 @@
+﻿using Atem.Core.Audio;
+using Atem.Core.Graphics;
+using Atem.Core.Input;
+using Atem.Core.Memory;
+using Atem.Core.Processing;
+using Atem.Core;
+
+namespace Atem.Factories
+{
+    public static class EmulatorFactory
+    {
+
+        public static Emulator Create()
+        {
+            Bus bus = new();
+            Processor processor = new(bus);
+            Interrupt interrupt = new();
+            Joypad joypad = new(interrupt);
+            Timer timer = new(interrupt);
+            SerialManager serial = new(interrupt);
+            AudioManager audio = new();
+            Cartridge cartridge = new();
+            GraphicsManager graphics = GraphicsManagerFactory.Create(bus, interrupt, cartridge);
+            bus.ProvideDependencies(processor, interrupt, joypad, timer, serial, graphics, audio, cartridge);
+            return new Emulator(bus, processor, interrupt, joypad, timer, serial, audio, cartridge, graphics);
+        }
+    }
+}
---
+++ src/Atem/Factories/GraphicsManagerFactory.cs
@@ -0,0 +1,29 @@
+﻿using Atem.Core;
+using Atem.Core.Graphics;
+using Atem.Core.Graphics.Interrupts;
+using Atem.Core.Graphics.Objects;
+using Atem.Core.Graphics.Palettes;
+using Atem.Core.Graphics.Screen;
+using Atem.Core.Graphics.Tiles;
+using Atem.Core.Graphics.Timing;
+using Atem.Core.Memory;
+using Atem.Core.Processing;
+
+namespace Atem.Factories
+{
+    public static class GraphicsManagerFactory
+    {
+
+        public static GraphicsManager Create(Bus bus, Interrupt interrupt, Cartridge cartridge)
+        {
+            RenderModeScheduler renderModeScheduler = new();
+            PaletteProvider paletteProvider = new();
+            HDMA hdma = new(bus, renderModeScheduler);
+            StatInterruptDispatcher statInterruptDispatcher = new(interrupt, renderModeScheduler);
+            TileManager tileManager = new(renderModeScheduler, paletteProvider, cartridge);
+            ObjectManager objectManager = new(bus, renderModeScheduler, tileManager, paletteProvider, cartridge);
+            ScreenRenderer screenRenderer = new(renderModeScheduler, tileManager, objectManager, cartridge);
+            return new GraphicsManager(interrupt, renderModeScheduler, paletteProvider, hdma, statInterruptDispatcher, tileManager, objectManager, screenRenderer);
+        }
+    }
+}
